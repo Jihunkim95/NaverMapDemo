266b31f5842da84193d3f918421f76b00ea94982
Jihunkim95 <darak551@naver.com>
2024-01-30 17:04:50 +0900

    init : 초기 설정

---
+++ NaverMapDemo/NaverMapDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaverMapDemoApp.swift
+//  NaverMapDemo
+//
+//  Created by 김지훈 on 2024/01/30.
+//
+
+import SwiftUI
+
+@main
+struct NaverMapDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
