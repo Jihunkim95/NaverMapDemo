70316c8be6d275a5ffced11b588d64688426281d
Jihunkim95 <darak551@naver.com>
2024-02-08 17:20:46 +0900

    [Add] : Cell클릭시 Contents내용 다음 화면 Title 적용

--- NaverMapDemo/View/ReportView.swift
+++ NaverMapDemo/View/ReportView.swift
@@ -10,36 +10,55 @@ import SwiftUI
 
 struct ReportView: View {
 
+    @State private var isTitleHidden: Bool = false
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        NavigationView{
+        NavigationStack{
             VStack{
-                    Text("해당 게시글을 신고하는 이유를 알려주세요.")
+                    Text("게시물 제목란:")
                     .bold()
                     .font(.system(size: 18))
                     .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 15)
-                    .padding(.trailing, 15)
-                    .padding(.top, 20)
-                    .padding(.bottom, 20)
+                    .padding(.horizontal, 21)
+                    .padding(.top, 15)
 
+
+                Text("해당 게시글을 신고하는 이유를 알려주세요.")
+                .bold()
+                .font(.system(size: 18))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 21)
+                                    .padding(.bottom, 15)
+                
                 Divider()
-                ReportLinkView(title: "허위/거짓 정보를 포함하고 있어요.", destination: AnyView(Text(".")) )
-                ReportLinkView(title: "욕설/비방/혐오 표현이 사용되었어요.", destination: AnyView(Text(".")) )
-                ReportLinkView(title: "선정적인 내용을 포함하고 있어요.", destination: AnyView(Text(".")) )
-                ReportLinkView(title: "기타", destination: AnyView(Text(".")) )
+                
+                NavigationLink(destination: ReportDetailView(title: "허위/거짓 정보를 포함하고 있어요.")){
+                    ReportCellView(contents: "허위/거짓 정보를 포함하고 있어요.").onDisappear(){
+                        isTitleHidden = true
+
+                    }
+                }
+                NavigationLink(destination: ReportDetailView(title: "욕설/비방/혐오 표현이 사용되었어요.")){
+                    ReportCellView(contents: "욕설/비방/혐오 표현이 사용되었어요.")
+                }
+                NavigationLink(destination: ReportDetailView(title: "선정적인 내용을 포함하고 있어요.")){
+                    ReportCellView(contents: "선정적인 내용을 포함하고 있어요.")
+                }
+                NavigationLink(destination: ReportDetailView(title: "기타")){
+                    ReportCellView(contents: "기타")
+                }
                 Spacer()
             }
-
         }.navigationBarTitle("신고하기", displayMode: .inline)
-            .navigationBarItems(leading: CustomBackButtonView())
-                 .navigationBarBackButtonHidden(true) // 뒤로 가기 버튼 숨기기
-
+        .navigationBarItems(leading: CustomBackButtonView())
+        .navigationBarBackButtonHidden(true) // 뒤로 가기 버튼 숨기기
 
     }
 }
 struct TestView1: View {
     var body: some View {
-        NavigationView{
+        NavigationStack{
             VStack{
                 NavigationLink(destination: ReportView()){
                     VStack{
@@ -65,14 +84,12 @@ struct CustomBackButtonView: View {
     }
 }
 
-struct ReportLinkView: View {
-    let title: String
-    let destination: AnyView // 실제 목적지 타입에 따라 변경 가능합니다.
+struct ReportCellView: View {
+    let contents: String
 
     var body: some View {
-        NavigationLink(destination: destination) {
             HStack {
-                Text(title)
+                Text(contents)
                     .padding(.vertical, 10)
                     .font(.system(size: 17))
                     .foregroundStyle(.black)
@@ -81,16 +98,16 @@ struct ReportLinkView: View {
                     .font(.system(size: 17))
                     .foregroundStyle(Color(hex: "3C3C43"))
             }
-            .padding(.horizontal, 15)
-        }
-        .frame(height: 40)
-        .background(
-            RoundedRectangle(cornerRadius: 0)
-                .foregroundColor(.white)
-                .shadow(color: Color.init(hex: "B3B3B3"), radius: 0, x: 0, y: 1)
-                .padding(.leading, 15)
-                .padding(.trailing, 15)
-        )
-        .padding(.top, 10)
+            .padding(.horizontal, 21)
+            .padding(.top, 10)
+            .frame(height: 40)
+            .background(
+                RoundedRectangle(cornerRadius: 0)
+                    .foregroundColor(.white)
+                    .shadow(color: Color.init(hex: "B3B3B3"), radius: 0, x: 0, y: 1)
+                    .padding(.leading, 21)
+                    .padding(.trailing, 21)
+            )
+
     }
 }
